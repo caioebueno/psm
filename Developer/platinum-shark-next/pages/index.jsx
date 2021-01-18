f3427b2223048d8356612b46621dd0eb66eab094
caio bueno <caioebueno@gmail.com>
2021-01-18 14:11:09 -0300

    done

---
+++ Developer/platinum-shark-next/pages/index.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable require-jsdoc */
+import React from 'react';
+
+import Home from './home/home';
+
+function Index(props) {
+  return (
+    <Home />
+  );
+}
+
+export default Index;
